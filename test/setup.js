40900c903a5d9874c28f584127f18c5f9438d1bd
Andreas Madsen <amwebdk@gmail.com>
2013-12-19 21:58:41 +0100

    [test] server should destory socket on bad type

---
+++ test/setup.js
@@ -0,0 +1,56 @@
+
+var fs = require('fs');
+var net = require('net');
+var test = require('tap').test;
+var path = require('path');
+var async = require('async');
+var wrench = require('wrench');
+var DailyServer = require('../daily-interface.js').Server;
+
+var DB_PATH = path.resolve(__dirname, 'temp.db');
+
+function ServerSetup() {
+  if (!(this instanceof ServerSetup)) return new ServerSetup();
+
+  if (fs.existsSync(DB_PATH)) wrench.rmdirSyncRecursive(DB_PATH);
+  this.server = null;
+  this.handler = null;
+  this.port = 0;
+}
+module.exports = ServerSetup;
+
+ServerSetup.prototype.open = function () {
+  var self = this;
+
+  test('open daily server', function (t) {
+    self.handler = new DailyServer(DB_PATH);
+
+    self.server = net.createServer(function (socket) {
+      self.handler.dispatch(socket);
+    });
+
+    self.server.listen(0, '127.0.0.1', function () {
+      self.port = self.server.address().port;
+      t.end();
+    });
+  });
+};
+
+ServerSetup.prototype.close = function () {
+  var self = this;
+
+  test('close daily server', function (t) {
+    async.parallel([
+      function (done) {
+        self.handler.once('close', done);
+        self.handler.close();
+      },
+      function (done) {
+        self.server.once('close', done);
+        self.server.close();
+      }
+    ], function () {
+      t.end();
+    });
+  });
+};
